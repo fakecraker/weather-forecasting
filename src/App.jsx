49d9eeca727d4f6c0f674db280eeb0fdc0159a8d
VARUN S <varunamirrthas@gamil.com>
2025-01-20 15:17:56 +0530

    again change on extension

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import axios from "axios";
-import Current from "./components/Current.jsx";
+import Current from "./Components/Current.jsx";
 import Forecast from "./Components/Forecast..jsx";
 import '../node_modules/bootstrap/dist/js/bootstrap'
 
